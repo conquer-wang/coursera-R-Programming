383f74622230d79e51419de1d0a61f165c7bda49
conquer-wang <60646222+conquer-wang@users.noreply.github.com>
2020-07-24 15:06:01 +0800

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,4 +1,4 @@
-%This function creates a special "matrix" object that can cache its inverse.
+# This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
